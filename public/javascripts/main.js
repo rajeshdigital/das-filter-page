1241c4a60b1b150e0e7df8e8f227681732806bb7
Rajesh Veluchamy <Rajesh@Rajeshs-MacBook-Pro.local>
2016-10-25 10:52:11 +0100

    filter page markup alteration and code cleanup to match GDS standards

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,13 @@
-// custom javascript file
+// Custom jquery code
 $(document).ready(function() {
 
-
+	// search filter page :: expand/collapse functionality
     $('.container-head').click(function() {
         $(this).toggleClass('showHide');
         $(this).next().toggleClass("hideOptions");  
 
-    })
+    });
+
 
 
 
